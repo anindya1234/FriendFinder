e8c8b48ebf9e94fef2b96b01550702da57e2d02b
anindya1234 <anindyagupta@email.arizona.edu>
2018-02-06 16:52:10 -0700

    V1

---
+++ data/friends.js
@@ -0,0 +1,27 @@
+ matchArray = [
+       {
+        name: "Tom Cruise",
+        photo: "http://images.indianexpress.com/2017/06/tom-cruise-759.jpg",
+        response: [
+                    "3","2",
+                    "5","4",
+                    "4","5",
+                    "1","2",
+                    "3","4"
+                ]
+        },
+        {
+        name: "Jenifer Lawrence",
+        photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BOTU3NDE5MDQ4MV5BMl5BanBnXkFtZTgwMzE5ODQ3MDI@._V1_UX214_CR0,0,214,317_AL_.jpg",
+        response: [
+                    "3","2",
+                    "4","4",
+                    "2","2",
+                    "1","1",
+                    "3","5"
+                ]
+        }
+
+];
+
+      module.exports = matchArray;
---
+++ routing/apiRoutes.js
@@ -0,0 +1,39 @@
+var friends = require("../data/friends");
+
+
+module.exports = function(app) {
+ 
+   app.get("/api/friend", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api", function(req, res) {
+    
+    
+    var resDifference=[];
+    
+
+    for(i=0;i<friends.length;i++){
+      //console.log(friends[i].response);
+      var difference=0;
+      for(j=0;j<10;j++){
+
+      //abs value of difference between responses for elements in array and new req
+        difference+=(Math.abs(parseInt(req.body.response[j])-parseInt(friends[i].response[j]))); 
+         //array containing difference in responses
+      }
+     resDifference.push(difference);
+    }
+    // console.log(resDifference);
+    // console.log(Math.min(...resDifference))
+
+    //    console.log(resDifference.indexOf(Math.min(...resDifference))); 
+    
+    res.json(friends[resDifference.indexOf(Math.min(...resDifference))]);
+   
+    friends.push(req.body);
+    
+  });
+
+ 
+};
